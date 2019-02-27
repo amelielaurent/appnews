94db6298ac4987bed32b1a430dc107af64c17024
amelie laurent <amelielaurent38@gmail.com>
2019-02-27 17:45:20 +0100

    Ajout carte google maps

--- app/src/main/java/com/newsgobelins/user/appnews/fragments/ContactFragment.java
+++ app/src/main/java/com/newsgobelins/user/appnews/fragments/ContactFragment.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.newsgobelins.user.appnews.R;
-import com.newsgobelins.user.appnews.listener.ArticleListener;
 
 import androidx.fragment.app.Fragment;
 
@@ -18,11 +17,13 @@ public class ContactFragment extends Fragment {
 
     private View view;
     private TextView mbuttonCall;
+    private TextView mbuttonAddress;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.contact_fragment, container, false);
         mbuttonCall = view.findViewById(R.id.contact_telephone);
+        mbuttonAddress = view.findViewById(R.id.contact_address);
         return view;
     }
 
@@ -37,6 +38,17 @@ public class ContactFragment extends Fragment {
                 Intent intent = new Intent(Intent.ACTION_DIAL);
                 intent.setData(Uri.parse("tel:0476542125"));
                 startActivity(intent);
+            }
+        });
+
+        //Fonction permettant d'afficher la carte
+        mbuttonAddress.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "VOIR LA CARTE", Toast.LENGTH_SHORT).show();
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=45.188460, 5.722344(Tabac Presse le 7/7, Grenoble)");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
             }
         });
     }
